2cb8f2eb2f1113f73694f809503ee530bfb790cb
Doug Miller <Djmiller1717@gmail.com>
2020-08-29 16:27:26 -0400

    need to seed data

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,3 +1,5 @@
+const woodworking = require('./db')
+const {Item} = require('./models/item')
 //Create real of fake seed date
 // if faker then install and require faker
 // create sync and seed file in db/index.js
