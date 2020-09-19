005509a5b68ec1bacc09b7a7a1540c8c57f3f5df
Doug Miller <Djmiller1717@gmail.com>
2020-09-18 22:43:52 -0400

    basic render failing - 404

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,33 @@
 import React from "react"
+import axios from 'axios'
 //import any sub-components
 
 export default class App extends React.Component {
 	//constructor to initialize state
 	constructor(){
 		super()
+		this.state = {
+			items: []
+		}
+	}
+	async componentDidMount(){
+		try {
+			const response = await axios.get('/api/items')
+			this.setState({items: response.data})
+		} catch (err) {
+			console.log(err)
+		}
+	}
+	render(){
+		return (
+			<div>
+				{this.state.items.map(item => {
+					return (
+					<h1>{item.name}</h1>
+					)
+				})}
+			</div>
+		)
 	}
 	//any lifecycle methods
 	//any custom methods
